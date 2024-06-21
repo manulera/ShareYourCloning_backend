2540f60ec6c240d67af3cb25111fc56376791b14
Manuel Lera-Ramirez <manulera14@gmail.com>
2024-06-21 14:38:37 +0100

    closes #139

--- primer_design.py
+++ primer_design.py
@@ -1,7 +1,7 @@
 from pydna.dseqrecord import Dseqrecord
 from pydna.design import primer_design
 from Bio.SeqFeature import SimpleLocation
-from pydna.utils import location_boundaries
+from pydna.utils import locations_overlap, shift_location, location_boundaries
 
 
 def homologous_recombination_primers(
@@ -15,12 +15,7 @@ def homologous_recombination_primers(
     target_tm: float,
 ) -> tuple[str, str]:
 
-    if hr_seq.circular:
-        # TODO: Conditions will have to be adapted for circular sequences, e.g. fwd_homology_start < 0
-        # is not enough
-        raise ValueError('Homology target sequence must be linear.')
-    start, end = location_boundaries(pcr_loc)
-    fragment2amplify = pcr_seq[start:end]
+    fragment2amplify = pcr_loc.extract(pcr_seq)
     amplicon = primer_design(fragment2amplify, limit=minimal_hybridization_length, target_tm=target_tm)
 
     if insert_forward:
@@ -31,14 +26,24 @@ def homologous_recombination_primers(
     if fwd_primer is None or rvs_primer is None:
         raise ValueError('Primers could not be designed, try changing settings.')
 
-    fwd_homology_start = hr_loc.start - homology_length
-    if fwd_homology_start < 0:
-        raise ValueError('Forward homology region is out of bounds.')
-    fwd_homology = hr_seq[fwd_homology_start : hr_loc.start]
+    hr_loc_start, hr_loc_end = location_boundaries(hr_loc)
+    fwd_homology_start = hr_loc_start - homology_length
+    rvs_homology_end = hr_loc_end + homology_length
 
-    rvs_homology_end = hr_loc.end + homology_length
-    if rvs_homology_end > len(hr_seq):
-        raise ValueError('Reverse homology region is out of bounds.')
-    rvs_homology = hr_seq[hr_loc.end : rvs_homology_end].reverse_complement()
+    if not hr_seq.circular:
+        if fwd_homology_start < 0:
+            raise ValueError('Forward homology region is out of bounds.')
+        if rvs_homology_end > len(hr_seq):
+            raise ValueError('Reverse homology region is out of bounds.')
+
+    # Convert to locations
+    fwd_arm = shift_location(SimpleLocation(fwd_homology_start, hr_loc_start), 0, len(hr_seq))
+    rvs_arm = shift_location(SimpleLocation(hr_loc_end, rvs_homology_end), 0, len(hr_seq))
+
+    if locations_overlap(fwd_arm, rvs_arm, len(hr_seq)):
+        raise ValueError('Homology arms overlap.')
+
+    fwd_homology = fwd_arm.extract(hr_seq)
+    rvs_homology = rvs_arm.extract(hr_seq).reverse_complement()
 
     return str(fwd_homology.seq).lower() + str(fwd_primer.seq), str(rvs_homology.seq).lower() + str(rvs_primer.seq)
