876f215c3363be8e766121a3175cf5fb054d25a1
Manuel Lera-Ramirez <manulera14@gmail.com>
2024-08-13 13:46:07 +0200

    make NCBI requests async

--- ncbi_requests.py
+++ ncbi_requests.py
@@ -1,8 +1,8 @@
 import requests
 from fastapi import HTTPException
-from urllib.error import HTTPError
-from pydna.genbank import Genbank
+from pydna.parsers import parse as pydna_parse
 import os
+from httpx import AsyncClient, Response
 
 NCBI_API_KEY = os.environ.get('NCBI_API_KEY')
 
@@ -10,6 +10,11 @@ if NCBI_API_KEY is None:
     headers = None  # pragma: no cover
 else:
     headers = {'api_key': NCBI_API_KEY}
+
+
+async def async_get(url, headers, params=None) -> Response:
+    async with AsyncClient(timeout=20.0) as client:
+        return await client.get(url, headers=headers, params=params)
 
 
 # TODO: this does not return old assembly accessions, see https://github.com/ncbi/datasets/issues/380#issuecomment-2231142816
@@ -25,10 +30,10 @@ def get_assembly_accession_from_sequence_accession(sequence_accession: str) -> l
         return []
 
 
-def get_sequence_accessions_from_assembly_accession(assembly_accession: str) -> list[str]:
+async def get_sequence_accessions_from_assembly_accession(assembly_accession: str) -> list[str]:
     """Get the sequence accessions from an assembly accession"""
     url = f'https://api.ncbi.nlm.nih.gov/datasets/v2alpha/genome/accession/{assembly_accession}/sequence_reports'
-    resp = requests.get(url, headers=headers)
+    resp = await async_get(url, headers=headers)
     data = resp.json()
     if 'reports' in data:
         return [report['refseq_accession'] for report in data['reports']] + [
@@ -40,9 +45,9 @@ def get_sequence_accessions_from_assembly_accession(assembly_accession: str) -> 
         raise HTTPException(404, 'Wrong assembly accession number')
 
 
-def get_annotation_from_locus_tag(locus_tag: str, assembly_accession: str) -> dict:
+async def get_annotation_from_locus_tag(locus_tag: str, assembly_accession: str) -> dict:
     url = f'https://api.ncbi.nlm.nih.gov/datasets/v2alpha/genome/accession/{assembly_accession}/annotation_report?search_text={locus_tag}'
-    resp = requests.get(url, headers=headers)
+    resp = await async_get(url, headers=headers)
     if resp.status_code == 404:
         raise HTTPException(404, 'wrong accession number')
     data = resp.json()
@@ -60,16 +65,30 @@ def get_annotation_from_locus_tag(locus_tag: str, assembly_accession: str) -> di
     return matching_annotations[0]
 
 
-def get_genbank_sequence_subset(sequence_accession, start, end, strand):
+async def get_genbank_sequence_subset(sequence_accession, start, end, strand):
     gb_strand = 1 if strand == 1 else 2
-    gb = Genbank('example@gmail.com')
-    try:
-        return gb.nucleotide(sequence_accession, start, end, gb_strand)
-    except HTTPError as e:
-        if e.code == 400:
-            raise HTTPException(404, 'wrong sequence accession')
-        else:
-            raise HTTPException(503, 'NCBI returned an error')
+    url = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi'
+    params = {
+        'db': 'nuccore',
+        'id': sequence_accession,
+        'rettype': 'gbwithparts',
+        'seq_start': start,
+        'seq_stop': end,
+        'strand': gb_strand,
+        'retmode': 'text',
+    }
+    if headers is not None:
+        params['api_key'] = headers['api_key']
+
+    resp = await async_get(url, headers=headers, params=params)
+    if resp.status_code == 200:
+        return pydna_parse(resp.text)[0]
+    elif resp.status_code == 400:
+        raise HTTPException(404, 'wrong sequence accession')
+    elif resp.status_code == 503:
+        raise HTTPException(503, 'NCBI returned an error')
+    else:
+        raise HTTPException(500, 'NCBI returned an unexpected error')
 
 
 def validate_coordinates_pre_request(start, end, strand):
